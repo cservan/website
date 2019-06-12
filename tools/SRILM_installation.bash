88d3ec8d47617a3c0af116a15bbbbb00dbbcfada
Christophe SERVAN <c.servan@qwant.com>
2019-06-12 19:51:53 +0200

    Easier version for installing SRILM

--- tools/SRILM_installation.bash
+++ tools/SRILM_installation.bash
@@ -1,6 +1,6 @@
 DESTINATION=/$HOME/Tools/SRILM-1.6.0
 
-
+rm -rf $DESTINATION
 mkdir -pv $DESTINATION
 echo "export PATH=\$PATH:$DESTINATION/bin/i686-m64/" >> ~/.bashrc
 echo "export SRILM=$DESTINATION" >> ~/.bashrc
@@ -8,9 +8,13 @@ echo "export SRILM=$DESTINATION" >> ~/.bashrc
 . ~/.bashrc
 
 pushd $DESTINATION
-	wget http://www-clips.imag.fr/geod/User/christophe.servan/tools/srilm-1.6.0.tar.gz
+	wget https://cservan.github.io/website/tools/srilm-1.6.0.tar.gz
 	tar xvfz srilm-1.6.0.tar.gz
+# for linux:    
 	SRILM=$DESTINATION make MACHINE_TYPE=i686-m64 World -j4
+# for macOS:
+#	SRILM=$DESTINATION make MACHINE_TYPE=macosx-m64 World -j4
+#
 popd
 
 . ~/.bashrc
